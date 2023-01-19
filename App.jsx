9342e8da7838d8a8786fe79e33725703aa239146
MyroslavTiuk <myroslavtiukwork@gmail.com>
2023-01-19 13:04:29 +0200

    first commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useEffect, useState } from 'react';
 import {
   Image,
@@ -15,50 +8,53 @@ import {
 } from 'react-native';
 
 function App() {
-  const [deck, setDeck] = useState({});
+  const [data, setData] = useState({});
   const [deckId, setDeckId] = useState("");
-  const [cards, setCards] = useState("");
-  const [showCards, setShowCards] = useState(false);
+  const [stack, setStack] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const requestId = async () => {
+    const request = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    const response = await request.json();
+    setData(response);
+  };
+
+  const requestCards = async () => {
+    const request = await fetch(`https://deckofcardsapi.com/api/deck/${deckId ? deckId : deck.deck_id}/draw/?count=4`);
+    const response = await request.json();
+    setStack(response);
+  };
 
   useEffect(() => {
-    const fetchDeck = async () => {
-      const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-      const data = await res.json();
-      setDeck(data);
-    }; fetchDeck();
-  }, [deck]);
+    requestId();
+  }, [data]);
 
   useEffect(() => {
-    const fetchCards = async () => {
-      const res = await fetch(`https://deckofcardsapi.com/api/deck/${deckId ? deckId : deck.deck_id}/draw/?count=4`);
-      const data = await res.json();
-      setCards(data);
-    }; fetchCards();
+    requestCards();
   }, [deckId]);
 
-  console.log(deck, "data");
-
-  const shufle = () => {
-    setDeckId(deck.deck_id);
-    setShowCards(false);
+  const shufleDeck = () => {
+    setDeckId(data.deck_id);
+    setVisible(false);
   };
 
   return (
     <View style={styles.sectionContainer}>
-      <TouchableOpacity onPress={() => shufle()}>
-        <Text>Shufle</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => { setShowCards(true); }}>
-        <Text>Draw</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonsArea}>
+        <TouchableOpacity style={styles.button} onPress={() => shufleDeck()}>
+          <Text>Shufle</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { setVisible(true); }}>
+          <Text>Show</Text>
+        </TouchableOpacity>
+      </View>
       {
-        showCards &&
+        visible &&
         <View style={styles.cardsBlock}>
-          {cards.cards?.map(item => {
+          {stack.cards?.map((item, index)=> {
             return (
-              <View>
-                <Text>{item.code}</Text>
-                <Image style={{ width: 60, objectFit: "fill", height: 90 }} source={{ uri: item.image }} />
+              <View key={index}>
+                <Image style={{ width: 70, objectFit: "contain", height: 90 }} source={{ uri: item.image }} />
               </View>
             );
           })}
@@ -74,22 +70,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: "center",
+    backgroundColor: "black",
+  },
+  buttonsArea: {
+    flexDirection: "row",
+    gap: 50,
+    marginBottom: 25
+  },
+  button:{
+    padding: 8,
+    backgroundColor: 'orange',
+    borderRadius: 12
   },
   cardsBlock: {
     flexDirection: "row",
     gap: 25,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
